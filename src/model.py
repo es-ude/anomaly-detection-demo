f5c9a2c52304efc883653602db767d6edaed0762
Julian Hoever <julianhoever@posteo.de>
2025-02-23 00:25:19 +0100

    reduce model depth
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- src/model.py
+++ src/model.py
@@ -1,45 +1,45 @@
-from typing import Any
-
 import torch
 from torch.nn import Conv2d, ConvTranspose2d, Flatten, Linear, ReLU, Unflatten
 
 
 class Encoder(torch.nn.Sequential):
     def __init__(self) -> None:
-        conv_kwargs: dict[str, Any] = dict(kernel_size=5, stride=2)
         super().__init__(
-            Conv2d(in_channels=3, out_channels=8, **conv_kwargs),
+            Conv2d(in_channels=3, out_channels=32, kernel_size=8, stride=4),
             ReLU(),
-            Conv2d(in_channels=8, out_channels=16, **conv_kwargs),
+            Conv2d(in_channels=32, out_channels=64, kernel_size=8, stride=4),
             ReLU(),
-            Conv2d(in_channels=16, out_channels=32, **conv_kwargs),
-            ReLU(),
-            Conv2d(in_channels=32, out_channels=64, **conv_kwargs),
+            Conv2d(in_channels=64, out_channels=128, kernel_size=8, stride=4),
             ReLU(),
             Flatten(),
-            Linear(in_features=10816, out_features=256),
+            Linear(in_features=512, out_features=128),
             ReLU(),
         )
 
 
 class Decoder(torch.nn.Sequential):
     def __init__(self) -> None:
-        conv_kwargs: dict[str, Any] = dict(kernel_size=5, stride=2)
         super().__init__(
-            Linear(in_features=256, out_features=10816),
+            Linear(in_features=128, out_features=512),
             ReLU(),
-            Unflatten(dim=1, unflattened_size=(64, 13, 13)),
-            ConvTranspose2d(in_channels=64, out_channels=32, **conv_kwargs),
-            ReLU(),
-            ConvTranspose2d(in_channels=32, out_channels=16, **conv_kwargs),
-            ReLU(),
+            Unflatten(dim=1, unflattened_size=(128, 2, 2)),
             ConvTranspose2d(
-                in_channels=16, out_channels=8, output_padding=1, **conv_kwargs
+                in_channels=128,
+                out_channels=64,
+                kernel_size=8,
+                stride=4,
+                output_padding=2,
             ),
             ReLU(),
             ConvTranspose2d(
-                in_channels=8, out_channels=3, output_padding=1, **conv_kwargs
+                in_channels=64,
+                out_channels=32,
+                kernel_size=8,
+                stride=4,
+                output_padding=3,
             ),
+            ReLU(),
+            ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=8, stride=4),
             ReLU(),
         )
 
