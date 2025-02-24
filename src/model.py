62f97cc6bccd665f5adceb0b4427902742f02131
Julian Hoever <julianhoever@posteo.de>
2025-02-24 15:10:37 +0100

    increase model capacity
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- src/model.py
+++ src/model.py
@@ -4,25 +4,25 @@ import torch
 class Encoder(torch.nn.Sequential):
     def __init__(self) -> None:
         super().__init__(
-            *_conv_block(in_channels=3, out_channels=32),  # 128
-            *_conv_block(in_channels=32, out_channels=64),  # 64
-            *_conv_block(in_channels=64, out_channels=128),  # 32
-            *_conv_block(in_channels=128, out_channels=256),  # 16
-            *_conv_block(in_channels=256, out_channels=512),  # 8
-            *_conv_block(in_channels=512, out_channels=1024),  # 4
+            *_conv_block(in_channels=3, out_channels=64),  # 128
+            *_conv_block(in_channels=64, out_channels=128),  # 64
+            *_conv_block(in_channels=128, out_channels=256),  # 32
+            *_conv_block(in_channels=256, out_channels=512),  # 16
+            *_conv_block(in_channels=512, out_channels=1024),  # 8
+            *_conv_block(in_channels=1024, out_channels=2048),  # 4
         )
 
 
 class Decoder(torch.nn.Sequential):
     def __init__(self) -> None:
         super().__init__(
-            *_deconv_block(in_channels=1024, out_channels=512, up_size=8),
-            *_deconv_block(in_channels=512, out_channels=256, up_size=16),
-            *_deconv_block(in_channels=256, out_channels=128, up_size=32),
-            *_deconv_block(in_channels=128, out_channels=64, up_size=64),
-            *_deconv_block(in_channels=64, out_channels=32, up_size=128),
+            *_deconv_block(in_channels=2048, out_channels=1024, up_size=8),
+            *_deconv_block(in_channels=1024, out_channels=512, up_size=16),
+            *_deconv_block(in_channels=512, out_channels=256, up_size=32),
+            *_deconv_block(in_channels=256, out_channels=128, up_size=64),
+            *_deconv_block(in_channels=128, out_channels=64, up_size=128),
             *_deconv_block(
-                in_channels=32, out_channels=3, up_size=256, final_layer=True
+                in_channels=64, out_channels=3, up_size=256, final_layer=True
             ),
         )
 
