bb5064fa9b3dca7e113e4ec4ae15ebc155e31d38
Julian Hoever <julianhoever@posteo.de>
2025-02-24 09:16:47 +0100

    updated autoencoder model
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- src/model.py
+++ src/model.py
@@ -1,46 +1,29 @@
 import torch
-from torch.nn import Conv2d, ConvTranspose2d, Flatten, Linear, ReLU, Unflatten
 
 
 class Encoder(torch.nn.Sequential):
     def __init__(self) -> None:
         super().__init__(
-            Conv2d(in_channels=3, out_channels=32, kernel_size=8, stride=4),
-            ReLU(),
-            Conv2d(in_channels=32, out_channels=64, kernel_size=8, stride=4),
-            ReLU(),
-            Conv2d(in_channels=64, out_channels=128, kernel_size=8, stride=4),
-            ReLU(),
-            Flatten(),
-            Linear(in_features=512, out_features=128),
-            ReLU(),
+            *_conv_block(in_channels=3, out_channels=64),  # 127
+            *_conv_block(in_channels=64, out_channels=64),  # 63
+            *_conv_block(in_channels=64, out_channels=128),  # 31
+            *_conv_block(in_channels=128, out_channels=128),  # 15
+            *_conv_block(in_channels=128, out_channels=256),  # 7
+            *_conv_block(in_channels=256, out_channels=256),  # 3
+            *_conv_block(in_channels=256, out_channels=32768),  # 1
         )
 
 
 class Decoder(torch.nn.Sequential):
     def __init__(self) -> None:
         super().__init__(
-            Linear(in_features=128, out_features=512),
-            ReLU(),
-            Unflatten(dim=1, unflattened_size=(128, 2, 2)),
-            ConvTranspose2d(
-                in_channels=128,
-                out_channels=64,
-                kernel_size=8,
-                stride=4,
-                output_padding=2,
-            ),
-            ReLU(),
-            ConvTranspose2d(
-                in_channels=64,
-                out_channels=32,
-                kernel_size=8,
-                stride=4,
-                output_padding=3,
-            ),
-            ReLU(),
-            ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=8, stride=4),
-            ReLU(),
+            *_deconv_block(in_channels=32768, out_channels=256, opad=0),
+            *_deconv_block(in_channels=256, out_channels=256, opad=0),
+            *_deconv_block(in_channels=256, out_channels=128, opad=0),
+            *_deconv_block(in_channels=128, out_channels=128, opad=0),
+            *_deconv_block(in_channels=128, out_channels=64, opad=0),
+            *_deconv_block(in_channels=64, out_channels=64, opad=0),
+            *_deconv_block(in_channels=64, out_channels=3, opad=1, final_layer=True),
         )
 
 
@@ -54,3 +37,46 @@ class Autoencoder(torch.nn.Module):
         encoded = self.encoder(input)
         decoded = self.decoder(encoded)
         return decoded
+
+
+def _conv_block(in_channels: int, out_channels: int) -> list[torch.nn.Module]:
+    return [
+        torch.nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=3,
+            stride=2,
+            groups=in_channels,
+        ),
+        torch.nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            groups=1,
+        ),
+        torch.nn.ReLU(),
+    ]
+
+
+def _deconv_block(
+    in_channels: int, out_channels: int, opad: int, final_layer: bool = False
+) -> list[torch.nn.Module]:
+    return [
+        torch.nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            groups=1,
+        ),
+        torch.nn.ConvTranspose2d(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            stride=2,
+            output_padding=opad,
+            groups=out_channels,
+        ),
+        torch.nn.Sigmoid() if final_layer else torch.nn.ReLU(),
+    ]
