462ad4989ebb975a348babacb94245a610d6adc5
Julian Hoever <julianhoever@posteo.de>
2025-03-10 14:18:20 +0100

    implement well-performing autoencoder

--- src/model.py
+++ src/model.py
@@ -4,20 +4,22 @@ import torch
 class Encoder(torch.nn.Sequential):
     def __init__(self) -> None:
         super().__init__(
-            *_conv_block(in_channels=1, out_channels=64),  # 64
-            *_conv_block(in_channels=64, out_channels=32),  # 32
-            *_conv_block(in_channels=32, out_channels=1),  # 16
+            *_conv_block(in_channels=1, out_channels=32),  # 64
+            *_conv_block(in_channels=32, out_channels=64),  # 32
+            *_conv_block(in_channels=64, out_channels=128),  # 16
+            *_conv_block(in_channels=128, out_channels=128),  # 8
         )
 
 
 class Decoder(torch.nn.Sequential):
     def __init__(self) -> None:
         super().__init__(
-            *_deconv_block(in_channels=1, out_channels=32, up_size=32),
-            *_deconv_block(in_channels=32, out_channels=64, up_size=64),
-            *_deconv_block(
-                in_channels=64, out_channels=1, up_size=128, final_layer=True
-            ),
+            *_deconv_block(in_channels=128, out_channels=128),  # 16
+            *_deconv_block(in_channels=128, out_channels=64),  # 32
+            *_deconv_block(in_channels=64, out_channels=32),  # 64
+            *_deconv_block(in_channels=32, out_channels=32),  # 128
+            torch.nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1),  # 128
+            torch.nn.ReLU(),
         )
 
 
@@ -33,67 +35,22 @@ class Autoencoder(torch.nn.Module):
         return decoded
 
 
-def _conv_block(
-    in_channels: int,
-    out_channels: int,
-    kernel_size: int = 3,
-    stride: int = 1,
-    pool_size: int = 2,
-) -> list[torch.nn.Module]:
+def _conv_block(in_channels: int, out_channels: int) -> list[torch.nn.Module]:
     return [
-        torch.nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=in_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding="same",
-            groups=in_channels,
-        ),
-        torch.nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=1,
-            stride=1,
-            groups=1,
-        ),
+        torch.nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=2, padding=2),
         torch.nn.ReLU(),
-        torch.nn.BatchNorm2d(num_features=out_channels),
-        torch.nn.MaxPool2d(kernel_size=pool_size),
     ]
 
 
-def _deconv_block(
-    in_channels: int,
-    out_channels: int,
-    up_size: int,
-    kernel_size: int = 3,
-    stride: int = 1,
-    final_layer: bool = False,
-) -> list[torch.nn.Module]:
-    if final_layer:
-        block_tail = [torch.nn.Sigmoid()]
-    else:
-        block_tail = [
-            torch.nn.BatchNorm2d(num_features=out_channels),
-            torch.nn.ReLU(),
-        ]
-
+def _deconv_block(in_channels: int, out_channels: int) -> list[torch.nn.Module]:
     return [
-        torch.nn.UpsamplingBilinear2d((up_size, up_size)),
-        torch.nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=in_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding="same",
-            groups=in_channels,
+        torch.nn.ConvTranspose2d(
+            in_channels,
+            out_channels,
+            kernel_size=5,
+            stride=2,
+            padding=2,
+            output_padding=1,
         ),
-        torch.nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=1,
-            stride=1,
-            groups=1,
-        ),
-        *block_tail,
+        torch.nn.ReLU(),
     ]
