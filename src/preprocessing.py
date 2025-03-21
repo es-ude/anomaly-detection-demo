38d3128d3fbe1bf621f1537f031a8bad63bd9217
Julian Hoever <julianhoever@posteo.de>
2025-03-21 16:42:22 +0100

    add histogram equalization

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,30 +1,22 @@
 import torch
-from torchvision.transforms.v2 import (
-    Compose,
-    Grayscale,
-    RandomHorizontalFlip,
-    RandomVerticalFlip,
-    Resize,
-    ToDtype,
-    ToImage,
-    Transform,
-)
+import torchvision.transforms.v2 as transforms
 
 
-class _BasePreprocessing(Compose):
+class _BasePreprocessing(transforms.Compose):
     def __init__(
         self,
         target_img_width: int,
         target_img_height: int,
-        augmentations: list[Transform],
+        augmentations: list[transforms.Transform],
     ) -> None:
         super().__init__(
             [
-                ToImage(),
-                Grayscale(num_output_channels=1),
-                Resize((target_img_width, target_img_height)),
+                transforms.ToImage(),
+                transforms.Grayscale(num_output_channels=1),
+                transforms.functional.equalize,
+                transforms.Resize((target_img_width, target_img_height)),
                 *augmentations,
-                ToDtype(dtype=torch.float32, scale=True),
+                transforms.ToDtype(dtype=torch.float32, scale=True),
             ]
         )
 
@@ -39,8 +31,8 @@ class TrainingPreprocessing(_BasePreprocessing):
             target_img_width,
             target_img_height,
             [
-                RandomHorizontalFlip(),
-                RandomVerticalFlip(),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
             ],
         )
 
