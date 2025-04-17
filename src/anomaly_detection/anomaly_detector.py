499c4b46754823a4c54df316cb9f01a9b50a5dcc
Julian Hoever <julianhoever@posteo.de>
2025-04-17 13:42:53 +0200

    minor renaming

--- src/anomaly_detection/anomaly_detector.py
+++ src/anomaly_detection/anomaly_detector.py
@@ -11,55 +11,58 @@ from src.anomaly_detection.model import Autoencoder
 from src.anomaly_detection.persistence import load_model
 from src.anomaly_detection.preprocessing import InferencePreprocessing
 
+type Image = npt.NDArray[np.uint8]
+
 
 @dataclass
 class DetectionResult:
-    original: npt.NDArray[np.uint8]
-    preprocessed: npt.NDArray[np.uint8]
-    reconstructed: npt.NDArray[np.uint8]
-    residuals: npt.NDArray[np.uint8]
-    result: npt.NDArray[np.uint8]
+    original: Image
+    preprocessed: Image
+    reconstructed: Image
+    residuals: Image
+    superimposed: Image
 
 
 class AnomalyDetector:
     def __init__(
         self,
         model_file: Path,
-        input_img_size: tuple[int, int],
-        inference_img_size: tuple[int, int],
+        input_image_size: tuple[int, int],
+        inference_image_size: tuple[int, int],
         device: torch.device = torch.device("cpu"),
     ) -> None:
-        self._model_file = model_file
-        self._input_img_size = list(input_img_size)
-        self._device = device
+        self.model_file = model_file
+        self.input_image_size = input_image_size
+        self.inference_image_size = inference_image_size
+        self.device = device
         self._model = None
-        self._preprocessing = InferencePreprocessing(*inference_img_size)
+        self._preprocessing = InferencePreprocessing(*inference_image_size)
 
     def load_model(self) -> None:
         self._model = Autoencoder()
-        load_model(self._model, self._model_file)
+        load_model(self._model, self.model_file)
         # self._model = torch.compile(self._model, backend="aot_eager")
         self._model.eval()
-        self._model.to(self._device)
+        self._model.to(self.device)
 
-    def detect(self, image: npt.NDArray[np.uint8]) -> DetectionResult:
+    def detect(self, image: Image) -> DetectionResult:
         if self._model is None:
             self.load_model()
 
-        preprocessed_image = self._preprocessing(image)
-        reconstructed_image = self._perform_inference(preprocessed_image)
-        residuals = _compute_residuals(preprocessed_image, reconstructed_image)
+        preprocessed = self._preprocessing(image)
+        reconstructed = self._perform_inference(preprocessed)
+        residuals = _compute_residuals(preprocessed, reconstructed)
         resized_residuals = self._resize_residuals(residuals)
         return DetectionResult(
             original=image,
-            preprocessed=_to_rgb(_to_numpy(preprocessed_image)),
-            reconstructed=_to_rgb(_to_numpy(reconstructed_image)),
+            preprocessed=_to_rgb(_to_numpy(preprocessed)),
+            reconstructed=_to_rgb(_to_numpy(reconstructed)),
             residuals=_apply_colormap(_to_numpy(residuals)),
-            result=_superimpose(image, _to_numpy(resized_residuals)),
+            superimposed=_superimpose(image, _to_numpy(resized_residuals)),
         )
 
     def _perform_inference(self, image: torch.Tensor) -> torch.Tensor:
-        image = image.to(self._device)
+        image = image.to(self.device)
         with torch.no_grad():
             result = self._model(image)
         return result.cpu()
@@ -67,33 +70,31 @@ class AnomalyDetector:
     def _resize_residuals(self, residuals: torch.Tensor) -> torch.Tensor:
         return tv_func.resize(
             residuals,
-            size=self._input_img_size,
+            size=list(self.input_image_size),
             interpolation=tv_func.InterpolationMode.BILINEAR,
         )
 
 
 def _compute_residuals(
-    original_image: torch.Tensor, reconstructed_image: torch.Tensor
+    original: torch.Tensor, reconstructed: torch.Tensor
 ) -> torch.Tensor:
-    return (original_image - reconstructed_image).abs()
+    return (original - reconstructed).abs()
 
 
-def _superimpose(
-    image: npt.NDArray[np.uint8], residuals: npt.NDArray[np.uint8]
-) -> npt.NDArray[np.uint8]:
+def _superimpose(image: Image, residuals: Image) -> Image:
     residuals = _apply_colormap(residuals)
     combined = cv2.addWeighted(image, 0.4, residuals, 0.6, 0)
     return cv2.cvtColor(combined, cv2.COLOR_RGBA2RGB)
 
 
-def _to_rgb(image: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
+def _to_rgb(image: Image) -> Image:
     return cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
 
 
-def _apply_colormap(image: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
+def _apply_colormap(image: Image) -> Image:
     heatmap_image = cv2.applyColorMap(image, cv2.COLORMAP_HOT)
     return cv2.cvtColor(heatmap_image, cv2.COLOR_BGR2RGB)
 
 
-def _to_numpy(image: torch.Tensor) -> npt.NDArray[np.uint8]:
+def _to_numpy(image: torch.Tensor) -> Image:
     return np.array(image.movedim(0, -1) * 255, dtype=np.uint8).squeeze()
