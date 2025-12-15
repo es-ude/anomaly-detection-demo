87ee7b33a5914eddcd2e2086b3daaf723c9f53fd
Julian Hoever <julianhoever@posteo.de>
2025-12-15 14:24:09 +0100

    modify classifier model and add script to train the classifer only
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- src/anomaly_detection/model.py
+++ src/anomaly_detection/model.py
@@ -39,22 +39,31 @@ class Autoencoder(torch.nn.Module):
 class Classifier(torch.nn.Sequential):
     def __init__(self) -> None:
         super().__init__(
-            torch.nn.Conv2d(
-                in_channels=128, out_channels=2, kernel_size=3, padding="same"
+            *_dwsep_conv(
+                in_channels=128, out_channels=16, kernel_size=3, padding="same"
             ),
-            torch.nn.MaxPool2d(kernel_size=8),
+            torch.nn.MaxPool2d(kernel_size=2),
+            torch.nn.ReLU(),
+            *_dwsep_conv(in_channels=16, out_channels=2, kernel_size=3, padding="same"),
+            torch.nn.AdaptiveAvgPool2d(output_size=1),
             torch.nn.Flatten(start_dim=-3),
         )
 
 
-def _conv_block(in_channels: int, out_channels: int) -> list[torch.nn.Module]:
+def _dwsep_conv(
+    in_channels: int,
+    out_channels: int,
+    kernel_size: int,
+    stride: int = 1,
+    padding: int | str = 0,
+) -> list[torch.nn.Module]:
     return [
         torch.nn.Conv2d(
             in_channels=in_channels,
             out_channels=in_channels,
-            kernel_size=5,
-            stride=2,
-            padding=2,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
             groups=in_channels,
         ),
         torch.nn.Conv2d(
@@ -64,6 +73,18 @@ def _conv_block(in_channels: int, out_channels: int) -> list[torch.nn.Module]:
             stride=1,
             padding=0,
             groups=1,
+        ),
+    ]
+
+
+def _conv_block(in_channels: int, out_channels: int) -> list[torch.nn.Module]:
+    return [
+        *_dwsep_conv(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=5,
+            stride=2,
+            padding=2,
         ),
         torch.nn.ReLU(),
     ]
