cb65104d1f5078065ae6bbe139fa6439db7da508
David P. Federl <david-peter.federl@uni-due.de>
2026-04-20 22:07:19 +0200

    feat: add link menu to page_layout

--- src/demo/web_app/pages/layout.py
+++ src/demo/web_app/pages/layout.py
@@ -22,3 +22,16 @@ def page_layout():
                 "flex-grow text-center text-4xl font-bold text-white"
             )
             ui.image(UDE_LOGO).props("width=200px height=50px fit=scale-down")
+
+    with ui.page_sticky(position="bottom-right", x_offset=18, y_offset=18):
+        with ui.button(icon="menu", color="dark").props("fab"):
+            with ui.menu().props("dark"):
+                ui.menu_item(
+                    "Demo", on_click=lambda: ui.navigate.to("/anomaly-detection")
+                )
+                ui.menu_item("Training", on_click=lambda: ui.navigate.to("/training"))
+                ui.separator()
+                ui.menu_item(
+                    "Calibration", on_click=lambda: ui.navigate.to("/calibration")
+                )
+                ui.menu_item("Preview", on_click=lambda: ui.navigate.to("/basic"))
